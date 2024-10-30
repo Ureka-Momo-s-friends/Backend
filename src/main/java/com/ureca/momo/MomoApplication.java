e0a56b8240866b7628b5f7ecb87ba3699afd87d2
hongsi0 <hongsizero@gmail.com>
2024-10-30 15:46:57 +0900

    Rebase-update

--- src/main/java/com/ureca/momo/MomoApplication.java
+++ src/main/java/com/ureca/momo/MomoApplication.java
@@ -2,12 +2,8 @@ package com.ureca.momo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.ureca.momo.domain")  // 엔티티 위치 지정
-@EnableJpaRepositories(basePackages = "com.ureca.momo.domain")  // 리포지토리 위치 지정
 public class MomoApplication {
 
 	public static void main(String[] args) {
